31b009e23e6c2e4e1845f7bb58055e59ce2c4509
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-17 12:10:30 +0700

    add new case

---
+++ app/service/exam1.js
@@ -0,0 +1,7 @@
+class Exam1 {
+  add(a, b) {
+    return (a * 1000 + b * 1000) / 1000;
+  }
+}
+const exam1 = new Exam1();
+module.exports = exam1;
--- tests/exam1.spec.js
+++ tests/exam1.spec.js
@@ -1,6 +1,18 @@
+const exam1 = require("../app/service/exam1");
+
 describe("Exam1", () => {
   test("case 1", () => {
     const data = ["One"];
     expect(data).toEqual(["One"]);
+  });
+
+  test("Should be 1 + 1 = 2", () => {
+    const result = exam1.add(1, 1);
+    expect(result).toEqual(2);
+  });
+
+  test("Should be 1.235 + 1 = 2.235", () => {
+    const result = exam1.add(1.235, 1);
+    expect(result).toEqual(2.235);
   });
 });
