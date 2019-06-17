799a6b20ee6de8ff7d6a1bde09bb124f6dc18992
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-17 13:12:19 +0700

    add helloWorldCallback

--- app/service/exam1.js
+++ app/service/exam1.js
@@ -2,6 +2,9 @@ class Exam1 {
   add(a, b) {
     return (a * 1000 + b * 1000) / 1000;
   }
+  helloWorldCallback(greeting) {
+    greeting("Hello", "World");
+  }
 }
 const exam1 = new Exam1();
 module.exports = exam1;
